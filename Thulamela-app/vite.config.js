054fccfa7e03e97e4d999ce1471ab0fdeea7bf7b
Ntokozo Sibiya <109238828+Xheeze@users.noreply.github.com>
2025-10-22 23:08:51 +0200

    Municipality loading
    Digital solution version1.002 React.js/css

---
+++ Thulamela-app/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@layouts': path.resolve(__dirname, './src/layouts'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@components': path.resolve(__dirname, './src/shared/components'),
+      '@stores': path.resolve(__dirname, './src/shared/stores')
+    }
+  }
+})
